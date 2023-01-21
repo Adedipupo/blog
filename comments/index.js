abb83d6ff5f712a1d4457b1dbe70568ea21465cf
Adedipupo <ajokanola@terragonltd.com>
2023-01-21 10:42:18 +0100

    first commit

---
+++ comments/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const {randomBytes} = require('crypto');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true,
+}));
+
+const commentsById = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsById[req.params.id] || []);
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+    const comments = commentsById[req.params.id] || [];
+
+    comments.push({id: commentId, content});
+
+    commentsById[req.params.id] = comments;
+
+    res.status(201).send(comments);
+})
+
+const port = 4001;
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+})
---
+++ posts/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const {randomBytes} = require('crypto');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true,
+}));
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+})
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+  const { title} = req.body;
+
+    posts[id] = {
+        id,
+        title,
+    }
+
+    res.status(201).send(posts[id]);
+})
+
+const port = 4000;
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+})
