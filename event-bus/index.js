c1f9fb5fc6a918d6b045bf91658f378ae35742ae
Adedipupo <ajokanola@terragonltd.com>
2023-02-09 10:16:08 +0100

    event-bus-server

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const axios = require('axios');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/event', (req, res){
+    const event = req.body
+
+    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4002/events', event);
+
+    res.status({ status : "OK"})
+})
+
+app.listen(4005, () => {
+    console.log('Listening on 4005')
+});
